4833e031735fbc4730932046af8cecbeb462e6be
Andrew <andrew@wellplayed.games>
2023-07-10 21:10:51 +0100

    Create reviews instead of individual comments

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 import {Context} from '@actions/github/lib/context'
 import {GitHub} from '@actions/github/lib/utils'
+import gitdiffParser from 'gitdiff-parser'
+import {ReplaceEpochTimes} from './util'
 
 async function run(): Promise<void> {
   try {
@@ -9,8 +11,12 @@ async function run(): Promise<void> {
     const githubToken = core.getInput('GITHUB_TOKEN')
     const minEpochString = core.getInput('minEpoch')
     const minEpoch = minEpochString ? parseInt(minEpochString) : 0
+    const maxLineLengthString = core.getInput('maxLineLength')
+    const maxLineLength = maxLineLengthString
+      ? parseInt(maxLineLengthString)
+      : 0
     if (githubToken) {
-      await processPR(githubToken, minEpoch)
+      await processPR(githubToken, minEpoch, maxLineLength)
     }
   } catch (error) {
     if (error instanceof Error) {
@@ -19,39 +25,42 @@ async function run(): Promise<void> {
   }
 }
 
-async function processPR(githubToken: string, minEpoch: number): Promise<void> {
+async function processPR(
+  githubToken: string,
+  minEpoch: number,
+  maxLineLength: number
+): Promise<void> {
   const context = github.context
   const octokit = github.getOctokit(githubToken)
+  await commentCommits(octokit, context, minEpoch, maxLineLength)
+}
 
-  await deleteThreads(octokit, context, filterThreadsFromAuthor, {
-    author: 'github-actions'
-  })
-
-  await commentCommits(octokit, context, minEpoch)
-
-  await deleteThreads(octokit, context, filterOutdatedThreadsFromAuthor, {
-    author: 'github-actions'
-  })
+type Comment = {
+  /** @description The relative path to the file that necessitates a review comment. */
+  path: string
+  /** @description The position in the diff where you want to add a review comment. Note this value is not the same as the line number in the file. For help finding the position value, read the note below. */
+  position?: number
+  /** @description Text of the review comment. */
+  body: string
+  /** @example 28 */
+  line?: number
+  /** @example RIGHT */
+  side?: string
+  /** @example 26 */
+  start_line?: number
+  /** @example LEFT */
+  start_side?: string
 }
 
 async function commentCommits(
   octokit: InstanceType<typeof GitHub>,
   context: Context,
-  minEpoch: number
+  minEpoch: number,
+  maxLineLength: number
 ): Promise<void> {
-  function replaceEpochTimes(match: string): string {
-    const epoch = parseInt(match)
-    if (epoch >= minEpoch) {
-      const date = new Date(0)
-      date.setUTCSeconds(epoch)
-      return date.toUTCString()
-    }
-    return match
-  }
-
   if (context.payload.pull_request) {
-    const commitList = await octokit.paginate(
-      octokit.rest.pulls.listCommits,
+    const reviews = await octokit.paginate(
+      octokit.rest.pulls.listReviews,
       {
         owner: context.repo.owner,
         repo: context.repo.repo,
@@ -59,222 +68,97 @@ async function commentCommits(
       },
       response => response.data
     )
-    const rawCommits = await Promise.all(
-      commitList.map(async commit =>
-        octokit.rest.repos.getCommit({
-          owner: context.repo.owner,
-          repo: context.repo.repo,
-          ref: commit.sha
-        })
+
+    const reviewIdsToClean = reviews
+      .filter(
+        review =>
+          review.user?.login === 'github-actions' &&
+          review.state === 'COMMENT' &&
+          review.body.startsWith('Commenting epoch timers')
       )
+      .map(review => review.id)
+    const reviewComments = await octokit.paginate(
+      octokit.rest.pulls.listReviewComments,
+      {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        pull_number: context.payload.pull_request.number
+      },
+      response => response.data
     )
-    const commits = rawCommits.map(rawCommit => rawCommit.data)
+    const commentsToDelete = reviewComments
+      .filter(
+        reviewComment =>
+          reviewComment.pull_request_review_id != null &&
+          reviewIdsToClean.includes(reviewComment.pull_request_review_id)
+      )
+      .map(reviewComment => reviewComment.id)
+    const commentDeletePromises = commentsToDelete.map(async commentId =>
+      octokit.rest.pulls.deleteReviewComment({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        comment_id: commentId
+      })
+    )
 
-    for (const commit of commits) {
-      if (commit.files) {
-        for (const file of commit.files) {
-          core.debug(`Processing ${file.filename}`)
-          const lines = file.patch?.split(/\r\n|\r|\n/)
-          if (lines) {
-            let rightLineNumber = 0
-            for (const line of lines) {
-              core.debug(`Processing ${line}`)
-              const lineNumbers = line.match(/@@ -(\d+),\d+ \+(\d+),\d+ @@/)
-              if (lineNumbers) {
-                rightLineNumber = parseInt(lineNumbers[2])
-              } else if (line.startsWith('+')) {
-                let comment = line.replace(/\d+/g, replaceEpochTimes)
-                if (comment !== line) {
-                  comment = comment.substring(1)
-                  core.debug(
-                    `Posting review comment to ${file.filename} - RIGHT - ${rightLineNumber}`
-                  )
-                  await octokit.rest.pulls.createReviewComment({
-                    owner: context.repo.owner,
-                    repo: context.repo.repo,
-                    pull_number: context.payload.pull_request.number,
-                    body: comment,
-                    path: file.filename,
-                    line: rightLineNumber,
-                    side: 'RIGHT',
-                    commit_id: commit.sha
-                  })
+    const diffResponse = await octokit.rest.pulls.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.payload.pull_request?.number,
+      headers: {
+        accept: 'application/vnd.github.diff'
+      }
+    })
+
+    const diff = diffResponse.data as unknown
+    if (typeof diff !== 'string') {
+      throw new Error('Unexpected type for diff')
+    }
+    const files = gitdiffParser.parse(diff)
+
+    const comments: Comment[] = []
+    for (const file of files) {
+      if (file.isBinary) {
+        continue
+      }
+      if (file.type === 'add' || file.type === 'modify') {
+        for (const hunk of file.hunks) {
+          for (const change of hunk.changes) {
+            if (change.type === 'insert') {
+              const commentBody = ReplaceEpochTimes(
+                change.content,
+                minEpoch,
+                maxLineLength
+              )
+              if (commentBody !== change.content) {
+                const comment = {
+                  path: file.newPath,
+                  body: commentBody,
+                  line: change.lineNumber,
+                  side: 'RIGHT'
                 }
-                rightLineNumber++
-              } else if (!line.startsWith('-')) {
-                rightLineNumber++
+                comments.push(comment)
               }
             }
           }
         }
       }
     }
-  }
-}
 
-async function deleteThreads(
-  octokit: InstanceType<typeof GitHub>,
-  context: Context,
-  filterFunction: (
-    reviewThread: ReviewThreadEdge,
-    filterParams: FilterThreadParams
-  ) => boolean,
-  params: FilterThreadParams
-): Promise<void> {
-  if (context.payload.pull_request) {
-    const reviewThreads = await getAllReviewThreadList(
-      octokit,
-      context.payload.pull_request.number
-    )
-    core.debug(JSON.stringify(reviewThreads))
-    const threadsToDelete = reviewThreads.filter(reviewThread =>
-      filterFunction(reviewThread, params)
-    )
-    core.debug(JSON.stringify(threadsToDelete))
-
-    const promises = []
-    for (const thread of threadsToDelete) {
-      for (const comment of thread.node.comments.edges) {
-        core.debug(`Deleting review comment ${comment.node.databaseId}`)
-        promises.push(
-          octokit.rest.pulls.deleteReviewComment({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            comment_id: comment.node.databaseId
-          })
-        )
-      }
+    if (comments.length > 0) {
+      core.debug('Posting review')
+      await octokit.rest.pulls.createReview({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        pull_number: context.payload.pull_request.number,
+        body: 'Commenting epoch timers',
+        event: 'COMMENT',
+        comments
+      })
     }
-    await Promise.all(promises)
-  }
-}
 
-function filterThreadsFromAuthor(
-  reviewThread: ReviewThreadEdge,
-  params: FilterThreadParams
-): boolean {
-  return reviewThread.node.comments.edges.every(
-    edge => edge.node.author.login === params.author
-  )
-}
-
-function filterOutdatedThreadsFromAuthor(
-  reviewThread: ReviewThreadEdge,
-  params: FilterThreadParams
-): boolean {
-  return (
-    reviewThread.node.isOutdated &&
-    filterThreadsFromAuthor(reviewThread, {author: params.author})
-  )
-}
-
-async function getAllReviewThreadList(
-  octokit: InstanceType<typeof GitHub>,
-  pullNumber: number
-): Promise<ReviewThreadEdge[]> {
-  const {edges: reviewThreads, page_info} = await getReviewThreadList(
-    octokit,
-    pullNumber,
-    null
-  )
-  if (page_info.hasNextPage) {
-    const res = await getReviewThreadList(
-      octokit,
-      pullNumber,
-      page_info.endCursor
-    )
-    return [...reviewThreads, ...res.edges]
-  }
-
-  return reviewThreads
-}
-
-type FilterThreadParams = {
-  author: string
-}
-
-type PageInfo = {
-  endCursor: string
-  hasNextPage: boolean
-}
-
-type ReviewThreadEdge = {
-  node: {
-    comments: {
-      edges: [
-        {
-          node: {
-            databaseId: number
-            author: {
-              login: string
-            }
-          }
-        }
-      ]
-    }
-    isOutdated: boolean
-  }
-}
-
-async function getReviewThreadList(
-  octokit: InstanceType<typeof GitHub>,
-  pullNumber: number,
-  cursor: string | null
-): Promise<{edges: ReviewThreadEdge[]; page_info: PageInfo}> {
-  const query = `
-    query GetReviewThreadList($repo_owner:String!, $repo_name:String!, $pull_request_number:Int!, $next_cursor:String){
-      repository(owner:$repo_owner, name:$repo_name) {
-        id
-        pullRequest(number:$pull_request_number) {
-          id
-          reviewThreads(first:100, after: $next_cursor) {
-            pageInfo{
-              hasNextPage
-              endCursor
-            }
-            edges {
-              node {
-                comments(first:2){
-                  edges{
-                    node{
-                      databaseId
-                      author{
-                        login
-                      }
-                    }
-                  }
-                }
-                isOutdated
-              }
-            }
-          }
-        }
-      }
-    }
-  `
-
-  type ReviewThreadsResponse = {
-    repository: {
-      pullRequest: {
-        reviewThreads: {
-          pageInfo: PageInfo
-          edges: ReviewThreadEdge[]
-        }
-      }
-    }
-  }
-
-  const parameter = {
-    repo_owner: github.context.repo.owner,
-    repo_name: github.context.repo.repo,
-    pull_request_number: pullNumber,
-    next_cursor: cursor
-  }
-
-  const result: ReviewThreadsResponse = await octokit.graphql(query, parameter)
-  return {
-    edges: result.repository.pullRequest.reviewThreads.edges,
-    page_info: result.repository.pullRequest.reviewThreads.pageInfo
+    core.debug('Deleting old comments')
+    await Promise.all(commentDeletePromises)
   }
 }
 
